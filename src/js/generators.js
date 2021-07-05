172ddd4687fa6b6d855261e99ca0c02bcde2cd8b
evgeniy zuev <evgn09@yandex.ru>
2021-07-05 22:04:13 +0300

    new2

--- src/js/generators.js
+++ src/js/generators.js
@@ -16,28 +16,37 @@ export function* characterGenerator(allowedTypes, maxLevel) {
   }
 }
 
-export function* classGenerator(options) {
-  for (let i = 0; i < options.length; i += 1) {
-    const option = options[i];
-    yield class extends Character {
-      constructor(level) {
-        super(level);
-        this.attack = option.attack;
-        this.defence = option.defence;
-        this.type = option.type;
-        this.atkradius = option.atkradius;
-        this.moveradius = option.moveradius;
-      }
-    };
-  }
+
+export function classGenerator(type, boardSize = 8) {
+  return new Array(boardSize).fill(0).reduce((acc, prev, index) => {
+    if (type ==='player') {
+      acc.push(index * boardSize, index * boardSize + 1);
+    } else {
+      acc.push(index * boardSize + boardSize -2, index * boardSize + boardSize - 1);
+    }
+    return acc;
+  }, []);
 }
 
-export function generateTeam(allowedTypes, maxLevel, characterCount) {
-  const newCharacters = [];
-  if (maxLevel === 1) { allowedTypes.splice(2); }
-  const characters = characterGenerator(allowedTypes, maxLevel);
-  for (let i = 0; i < characterCount; i += 1) {
-    newCharacters.push(characters.next().value);
+
+export function generateTeam(allowedTypes, maxLevel, characterCount, boardSize) {
+  const playerCoordinates = classGenerator('player', boardSize);
+  const npcCoordinates = classGenerator('npc', boardSize);
+  let position;
+  let idx;
+  const teams = [];
+  for (let key = 0; key < characterCount; key += 1) {
+    const { value } = characterGenerator(allowedTypes, maxLevel).next();
+    if (value.isPlayer) {
+      idx = Math.floor(Math.random() * playerCoordinates.length);
+      position = playerCoordinates[idx];
+      playerCoordinates.splice(idx, 1);
+    } else {
+      idx = Math.floor(Math.random() * npcCoordinates.length);
+      position = npcCoordinates[idx];
+      npcCoordinates.splice(idx, 1);
+    }
+    teams.push(new PositionedCharacter(value, position));
   }
-  return newCharacters;
+  return teams;
 }
